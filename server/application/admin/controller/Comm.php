4cc7cb4c68e994620a41fdf61beab1e770fdab3a
master-nan <305062463@qq.com>
2018-10-13 11:11:21 +0800

    update

--- server/application/admin/controller/Comm.php
+++ server/application/admin/controller/Comm.php
@@ -22,7 +22,7 @@ class Comm extends Controller
     public function checkToken()
     {
         $this->user = json_decode(cache($this->token), true);
-        if (!$this->user || $this->user == 1) {
+        if (!is_array($this->user)) {
             exit(json_encode(['code'=>101, 'error'=>'请重新登录']));
         }
         //每次访问自动续命
